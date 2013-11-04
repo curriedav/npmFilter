c6ae4e04817bbec82f3e8b6eab706dd673575c1d
curriedav <curriedav@gmail.com>
2013-11-04 09:23:52 -0800

    functioning npm-download-count

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var request = require('request');
-request('http://registry.npmjs.org/', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(JSON.parse(body)) // Print the google web page.
-  }
-})
+const assert = require('assert')
+    , moment = require('moment')
+    , downloadCounts = require('npm-download-counts')
 
+    , pkg    = 'levelup'
+    , start  = moment().subtract('months', 1).toDate() // start date for lookup
+    , end    = new Date()                              // end date for lookup
+
+downloadCounts(
+    pkg
+  , start
+  , end
+  , function (err, data) {
+      // `data` is an array of objects with `day` and `count` properties
+      // each element of the array represents a day in your date range
+      data.forEach(function (d, i) {
+        console.log('On %s, %s was downloaded ~%d times', d.day, pkg, d.count)
+      })
+    }
+)
